652b896439038caefd2ef09103f28f755e921fa3
kyumin0903 <kmkm0903@dgu.ac.kr>
2024-05-01 18:04:05 +0900

    feat: frontend 틀 구현

---
+++ Src/frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from "./pages/Login/Login.jsx";
+import { Join } from "./pages/Join/Join.jsx";
+import { Category } from "./pages/Category/Category.jsx";
+import { Main } from "./pages/Main/Main.jsx";
+import { Bookmark } from "./pages/Bookmark/Bookmark.jsx";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/join" element={<Join />} />
+      <Route path="/category" element={<Category />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/bookmark" element={<Bookmark />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
