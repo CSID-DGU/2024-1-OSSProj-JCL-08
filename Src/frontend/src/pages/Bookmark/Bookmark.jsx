965d260ecf9a17f6bbb0dbe7f4093d054a243e6d
kyumin0903 <kmkm0903@dgu.ac.kr>
2024-05-27 18:03:42 +0900

    fix: 오류수정

--- Src/frontend/src/pages/Bookmark/Bookmark.jsx
+++ Src/frontend/src/pages/Bookmark/Bookmark.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export const Bookmark = () => {
-  const { bookmarkedNews, removeBookmark } = useBookmarks();
 
     return (
 <div> hello</div>
